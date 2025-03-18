6959024bb4321881ca8d1ab6e8799e4fa550c566
zainysf <zaiyou51@gmail.com>
2025-03-18 14:49:27 +0100

    Add files via upload

--- restauarant order analysis.sql
+++ restauarant order analysis.sql
@@ -24,5 +24,47 @@ select category, avg(price) as 'avg_price'
 from Menu_items
 group by category
 
+select * from order_details
+-- 1- Combine the menu_items table and order_details table into a single table.
+select *
+from menu_items m join order_details d
+     on m.menu_item_id = d.item_id;
+     
+-- 2- What were the least and most ordered items? What category were they in? 
+select item_name, category, count(order_details_id) as num_of_purchases
+from order_details d left join menu_items m
+     on d.item_id = m.menu_item_id 
+group by item_name, category
+order by num_of_purchases;
+
+-- 3- What were the top 5 orders that spent the most money
+select order_id, sum(price) as 'total_spend'
+from order_details d left join menu_items m
+     on d.item_id = m.menu_item_id
+group by order_id
+order by total_spend desc
+limit 5;
+
+     
+-- 4- View the details of the highest spent order. What insights can you gather from the results?
+select category, count(item_id) as 'num_of_items'
+from order_details d left join menu_items m
+     on d.item_id = m.menu_item_id
+where order_id = 440     
+group by category;
+-- the highest spend order bought a lot of items from the italian category
+
+
+-- 5- View the details of the top 5 highest spent order. What insights can you gather
+select category, count(item_id) as 'num_of_items'
+from order_details d left join menu_items m
+     on d.item_id = m.menu_item_id
+where order_id in (440, 275, 1957, 330, 2675)     
+group by category;
+-- italian food is purchased the most in the top 5 highest spend orders
+
+
+
+
 
 
